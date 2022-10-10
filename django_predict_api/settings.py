f1b43f3dbd395463c5eadf1cf0f209c30a9dc7b3
RessMatthew <1372108577@qq.com>
2022-10-10 12:17:30 +0800

    add security secret_key

--- django_predict_api/settings.py
+++ django_predict_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9=iwgc%7=h$i7rw^40%62u3id&pur5jf$lty)s$i!1-sqv$u$f"
+SECRET_KEY = " "  # django SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
